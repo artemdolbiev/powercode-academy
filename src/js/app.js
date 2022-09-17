9a282149a32f0e66bf7be69812b1a2cc481ee87d
Artem Dolbiev <dolbievartem@gmail.com>
2022-09-17 14:51:41 +0300

    Popup

--- src/js/app.js
+++ src/js/app.js
@@ -5,5 +5,37 @@ menuInit();
 
 // Popup
 
+'use strict';
+
+const popupWindow = document.querySelector('.popup');
+const overlay = document.querySelector('.overlay');
+const btnClocePopupWindow = document.querySelector('.close-popup');
+const btnsShowPopupWindow = document.querySelectorAll('.popup-link');
+console.log(btnsShowPopupWindow);
+
+const showPopupWindow = function() {
+  popupWindow.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+const closePopupWindow = function() {
+  popupWindow.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsShowPopupWindow.length; i++) {
+  btnsShowPopupWindow[i].addEventListener('click', showPopupWindow);
+}
+
+btnClocePopupWindow.addEventListener('click', closePopupWindow);
+overlay.addEventListener('click', closePopupWindow);
+
+document.addEventListener('keydown', function(e) {
+  console.log(e);
+
+  if (e.key === 'Escape' && !popupWindow.classList.contains('hidden')) {
+    closePopupWindow();
+  }
+})
 
 
